fe0165680ceb2d8d38dd977d6cf524c0aaa32957
wxhami <d18102002@mail.ru>
2023-07-05 22:05:47 +0300

    add GetIndexes method

--- src/TaskKamino/Program.cs
+++ src/TaskKamino/Program.cs
@@ -1,3 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿var nums = new  List <int> () {2, 7, 11, 15, 9};
+int target = 9;
+GetIndexes(nums, 9);
 
-Console.WriteLine("Hello, World!");
+(int, int) GetIndexes(List<int> nums, int target)
+{
+    for (int i = 0; i < nums.Count; i++)
+    {
+        for (int j = 0; j < nums.Count; j++)
+        {
+            if (i == j)
+            {
+                continue;
+            }
+            else
+            {
+
+                int first = nums[i];
+                int second = nums[j];
+                var sum = first + second;
+                if (sum == target)
+                {
+                    Console.WriteLine(i);
+                    Console.WriteLine(j);
+                    return (first, second);
+
+                }
+            }
+        }
+    }
+
+    throw new Exception("Error");
+}
