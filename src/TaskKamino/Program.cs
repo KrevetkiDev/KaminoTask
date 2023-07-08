af576667b83a6564c526692689ee4c546c802f90
wxhami <d18102002@mail.ru>
2023-07-08 13:32:02 +0300

    add README

--- src/TaskKamino/Program.cs
+++ src/TaskKamino/Program.cs
@@ -1,6 +1,6 @@
 ﻿using TaskKamino;
 
-var nums = new  List <int> () {2, 7, 11, 15, 9};
+var nums = new List<int>() { 2, 7, 11, 15, 9 };
 int target = 9;
-ArrayUtils.GetIndexes(nums.ToArray(), 9);
 
+ArrayUtils.GetIndexes(nums.ToArray(), 9);
